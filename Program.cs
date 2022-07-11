a60d55f1d085d531405a6a3d6f041ab7469fcd34
Ilya Popov <pils78@mail.ru>
2022-07-11 14:54:17 +0300

    задача №2

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int a=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("max a");
+
+}
+else
+{
+    Console.WriteLine("max b");
+}  
